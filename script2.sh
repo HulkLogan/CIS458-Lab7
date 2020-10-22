1a3b89ac7b1c964e537a6bc0919c9ed199cf5e71
Annelise Trout <trouta@mail.gvsu.edu>
2020-10-22 16:56:53 -0400

    initialized secondary script to create alias

---
+++ script2.sh
@@ -0,0 +1,3 @@
+# Create alias where 'yay' is the alias to be used and 'script.sh' is the file overwrite program
+echo "alias yay='/home/cybersecurity/Desktop/cis458labs/lab07/CIS458-Lab7/script.sh'" >> ~/.bashrc
+source ~/.bashrc
